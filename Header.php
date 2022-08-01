19b02f51c6ec5c319b811bb70a362684eb9648dd
ANJWICLINTON123 <clintonanjwi@gmail.com>
2022-08-01 12:11:50 +0100

    modified

--- Header.php
+++ Header.php
@@ -7,7 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="style_grid.css">
     <link rel="stylesheet"  href="index.css">
-    <script src="Homepage.js"></script>
+    <script src="js_file.js"></script>
 </head>
 <body>
   
@@ -45,19 +45,6 @@
     <a class = "login1" href="login.php">login</a>
 </div>
 
-
-<!-- ########################## OPen and Close javascrip code ################################### -->
-
-<script>
-    function openSlideMenu(){
-          document.getElementById('side-menu').style.width = '250px';
-          document.getElementById('main').style.marginLeft = '250px';
-      }
-      function closeSlidemenu(){
-          document.getElementById('side-menu').style.width = '0';
-          document.getElementById('main').style.marginLeft = '0';
-      }
-</script>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
