efe7a2ccfedb33a30fa350e520bdbdb9d4c84f4d
ANJWICLINTON123 <clintonanjwi@gmail.com>
2022-08-01 12:10:19 +0100

    my javascript file

---
+++ js_file.js
@@ -0,0 +1,10 @@
+// ####################### for header ###########################
+
+function openSlideMenu(){
+      document.getElementById('side-menu').style.width = '250px';
+      document.getElementById('main').style.marginLeft = '250px';
+  }
+  function closeSlidemenu(){
+      document.getElementById('side-menu').style.width = '0';
+      document.getElementById('main').style.marginLeft = '0';
+  }
