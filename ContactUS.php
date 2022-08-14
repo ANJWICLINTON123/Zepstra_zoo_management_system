9a3fa4d97e30b3199199fedeaa5bc6440e0ee310
ANJWICLINTON123 <clintonanjwi@gmail.com>
2022-08-14 15:07:22 +0100

    modified

--- ContactUS.php
+++ ContactUS.php
@@ -53,18 +53,90 @@
                to you with an answer shortly. We will 
               respond to your email request in 24 - 48 business hours.</p>
  <!-- ########################## contact us form ################################### -->
+ <?php
+// define variables and set to empty values
+$nameErr = $emailErr= "";
+$name = $email = $subject = $comment = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "<br> Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr ="<br>Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "<br> Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "<br> Invalid email format";
+    }
+  }
+    
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["subject"])) {
+    $subject = "";
+  } else {
+    $subject = test_input($_POST["subject"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+require('mysql_connection.php');
+
+  if(isset($_POST['email'])) { 
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $subjects = $_REQUEST['subject'];
+    $messages = $_REQUEST['messages'];
+    
+    $sql = "INSERT INTO messagess (name, email, subjects, messages)
+    VALUES ('$name',
+  '$email','$subjects','$messages')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<span class= 'message'>New record created successfully</span>";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+  $conn->close();
+
+  }
+?>
 
         <div class="SubmitForm">
         <h1 class="foreign_ticket">Send your Message</h1>
-            <form action="">
-                <label for="">Name</label>  <input type="text" id="lname" name="lastname" placeholder="Your last name.."><br><br>
-                <label for="">Email</label>  <input type="Email" id="email" name="email" placeholder="enter email.."><br><br>
-                <label for="">Subject</label> <input type="text" id="subject" name="subject" placeholder=""><br><br>
-                <textarea class="textarea" id="w3review" name="w3review" rows="4" cols="50" placeholder="type your message here"></textarea>
-                <br>
-                
-                <button class="submit_button">Submit</button>
-            </form>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+       Name: <input type="text" name="name" placeholder="Your last name.." value="<?php echo $name;?>">
+       <span class="error"> <?php echo $nameErr;?></span>
+        <br><br>
+        E-mail: <input type="text" name="email" placeholder="enter email.." value="<?php echo $email;?>">
+       <span class="error"> <?php echo $emailErr;?></span>
+       <br><br>
+       <label for="">Subject</label> <input type="text" id="subject" name="subject"value="<?php echo $subject;?>" required><br><br>
+       <textarea class="textarea" id="w3review" name="messages" rows="4" cols="50" placeholder="type your message here"><?php echo $comment;?></textarea>
+       <br><br>
+         <input class ="submitButton" type="submit" name="submit" value="Submit">  
+   </form>
         </div>
         </div>
       </div>
